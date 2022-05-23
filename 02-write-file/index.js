d3f055eef41dfbbedbbf5b74f022419b29d6bc5a
Salohiddin98 <salohiddin.rashidov98@gmail.com>
2022-05-23 19:59:54 +0500

    feat: solve 1,2 tasks

---
+++ 02-write-file/index.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const { stdin, stdout } = process;
+
+const text = fs.createWriteStream("02-write-file/text.txt");
+stdout.write("Введите что душе угодно:\n");
+stdin.on("data", (data) => {
+  if (data.toString() === "exit\r\n") {
+    process.exit();
+  } else if (process.on("SIGINT", () => process.exit()));
+  text.write(data);
+});
+
+process.on("exit", () => {
+  stdout.write("Пока!");
+});
