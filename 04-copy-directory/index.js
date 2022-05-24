41b29120d14eb4cd5b0d3d2bac5b2807bda844eb
Salohiddin98 <salohiddin.rashidov98@gmail.com>
2022-05-24 16:29:23 +0500

    feat: solve 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+const dir = path.join(__dirname, "files");
+const dest = path.join(__dirname, "files-copy");
 
+fs.readdir(dest, { withFileTypes: true }, (err, items) => {
+  if (err) throw err;
+  items.forEach((file) => {
+    if (file.isFile()) {
+      fs.unlink(`${dest}/${file.name}`, (err) => err);
+    }
+  });
+});
+
+fs.mkdir(dest, { recursive: true }, (err) => {
+  if (err) throw err;
+  copyDir();
+});
+
+function copyDir() {
+  fs.readdir(dir, { withFileTypes: true }, (err, items) => {
+    if (err) throw err;
+    items.forEach((file) => {
+      if (file.isFile()) {
+        fs.copyFile(`${dir}/${file.name}`, `${dest}/${file.name}`, (err) => {
+          if (err) {
+            throw err;
+          }
+        });
+      } else {
+        console.log(file.name + "-папка и не будет скопирован!");
+      }
+    });
+  });
+}
